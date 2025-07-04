4d1707343c76f2852bcbbd0fde5f5ffe2ca26c21
Luccas Hessel <luccashessel@gmail.com>
2025-07-04 16:54:23 -0300

    Update conserto duplo hash.

--- createUser.js
+++ createUser.js
@@ -3,8 +3,7 @@ const bcrypt = require("bcrypt");
 const User = require("./models/User");
 
 mongoose.connect("mongodb://127.0.0.1:27017/ecommerceDB").then(async () => {
-    const hashedPassword = await bcrypt.hash("123456", 10);
-    const user = new User({ email: "teste@email.com", password: hashedPassword });
+    const user = new User({ email: "teste2@email.com", password: "121212" });
     await user.save();
     console.log("Usuário criado com sucesso!");
     process.exit();
